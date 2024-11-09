63c8e7dcf006aea938e24ff9485b8f29a390651b
DacHaiPham <hai.for.work4320@gmail.com>
2024-11-09 19:39:01 +0700

    Module2

---
+++ src/thucHanh_IntroductionToJava/useVariable.java
@@ -0,0 +1,20 @@
+package thucHanh_IntroductionToJava;
+
+public class useVariable {
+    public static void main(String[] args) {
+        int i = 5;
+        float f = 25.0f;
+        double d = 25.0;
+        boolean b = true;
+        char c = 'c';
+        String s = "Đà Nẵng";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+
+}
